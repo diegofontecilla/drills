c58ed9e3228a89becd6857801fef26dfcf06a807
Sam Morgan <s_morgan@me.com>
2018-05-14 01:06:36 +0100

    Remind the user to go check the leaderboard once they're done

--- lib/reporter.rb
+++ lib/reporter.rb
@@ -6,7 +6,13 @@ Dotenv.load(ENV['ENVIRONMENT'] == 'test' ? '.env.test' : '../.env')
 class Reporter
   URL = URI("#{ENV['LEADERBOARD_URI']}/times").freeze
 
+  def initialize(output = STDOUT)
+    @output = output
+  end
+
   def post_time(time)
     Net::HTTP.post_form(URL, 'time' => "#{time}")
+
+    @output.puts "Your time was posted to the leaderboard! Check it out here: #{ENV['LEADERBOARD_URI']}"
   end
 end
--- spec/reporter_spec.rb
+++ spec/reporter_spec.rb
@@ -1,12 +1,22 @@
 require 'reporter'
 
 RSpec.describe Reporter do
-  subject(:reporter) { described_class.new }
+  subject(:reporter) { described_class.new(fake_output) }
+  let(:fake_output)  { double(puts: nil) }
 
   describe '#post_time' do
     it 'posts a time to the leaderboard' do
       url = URI('http://localhost:9292/times')
       expect(Net::HTTP).to receive(:post_form).with(url, 'time' => '0.1')
+
+      reporter.post_time(0.1)
+    end
+
+    it 'reminds the user to go see how they did' do
+      url = URI('http://localhost:9292/times')
+      allow(Net::HTTP).to receive(:post_form)
+
+      expect(fake_output).to receive(:puts).with("Your time was posted to the leaderboard! Check it out here: http://localhost:9292")
 
       reporter.post_time(0.1)
     end
